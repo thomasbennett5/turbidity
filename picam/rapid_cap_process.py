1f64916d4b73a7ed20b2e131b21a4fb3ff3cf0b8
Tom Bennett <thomas.bennett5@gmail.com>
2019-11-28 17:07:24 +0000

    picam folder made, rapid capture and process script added

---
+++ picam/rapid_cap_process.py
@@ -0,0 +1,59 @@
+# Pi Camera class for capturing and previewing images for analysis
+
+
+import io
+import logging
+
+from PIL import Image
+
+from picamera import PiCamera
+
+from .CameraInterface import CameraInterface
+
+
+class CameraPicamera(CameraInterface):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.hasPreview = True
+        self.hasIdle = True
+
+        logging.info('Using PiCamera')
+
+        self._cap = None
+
+        self.setActive()
+        self._preview_resolution = (self._cap.resolution[0] // 2,
+                                    self._cap.resolution[1] // 2)
+        self.setIdle()
+
+    def setActive(self):
+
+        if self._cap is None or self._cap.closed:
+            self._cap = PiCamera()
+
+    def setIdle(self):
+
+        if self._cap is not None and not self._cap.closed:
+            self._cap.close()
+            self._cap = None
+
+    def getPreview(self):
+
+        self.setActive()
+        stream = io.BytesIO()
+        self._cap.capture(stream, format='jpeg', use_video_port=True,
+                          resize=self._preview_resolution)
+        stream.seek(0)
+        return Image.open(stream)
+
+    def getPicture(self):
+
+        self.setActive()
+        stream = io.BytesIO()
+        self._cap.capture(stream, format='jpeg', resize=None)
+        stream.seek(0)
+        return Image.open(stream)
+© 2019 GitHub, Inc.
