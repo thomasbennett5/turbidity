74faed05d7b40b5f29c1b50db361909f74608ef7
Tom Bennett <thomas.bennett5@gmail.com>
2019-11-28 13:51:01 +0000

    webcam plot add rectangle patch

--- webcam/webcam_capture.py
+++ webcam/webcam_capture.py
@@ -49,12 +49,25 @@ with picamera.PiCamera() as camera:
 
 img_arr = stream.rgb_array
 
+y0 = 350
+y1 = 480
+x0 = 180
+x1 = 650
 
+whiteArea_start = x0
+whiteArea_stop  = x0 + 100
+whiteArea_width = whiteArea_stop - whiteArea_start
+whiteArea_height= y1 - y0
+
+blackArea_start = x0 + 200
+blackArea_stop  = x0 + 300
+blackArea_width = blackArea_stop - blackArea_start
+blackArea_height= y1 - y0
 
 aoi = img_arr[350:480,180:650,0]
 
-whiteArea = np.average(aoi[:,:100])
-blackArea = np.average(aoi[:,200:300])
+whiteArea_avg = np.average(img_arr[y0:y1, whiteArea_start:whiteArea_stop])
+blackArea_avg = np.average(img_arr[y0:y1, blackArea_start:blackArea_stop])
 
 print "White area average : ", whiteArea
 print "Black area average : ", blackArea
@@ -63,7 +76,10 @@ print "Difference         : ", whiteArea - blackArea
 fig = plt.figure()
 
 img = fig.add_subplot(2,1,1)
-img.imshow(img_arr[350:480,180:650,0])
+img.imshow(img_arr)
+
+rect = patches.Rectangle((whiteArea_start,y0),whiteArea_width,whiteArea_height,linewidth=1,edgecolor='r',facecolor='none')
+img.add_patch(rect)
 
 text = fig.add_subplot(2,1,2)
 text.get_xaxis().set_visible(False)
