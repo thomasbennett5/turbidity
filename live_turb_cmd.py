04ef7d2791f2f4fbe3aa2ba78b83bbab40a59f20
Tom Bennett <thomas.bennett5@gmail.com>
2019-12-06 10:37:40 +0000

    added averaging for data acquisition in live_turbidy_cmd.py

--- live_turb_cmd.py
+++ live_turb_cmd.py
@@ -87,7 +87,7 @@ while True:
     turb_res = turb_avg / buffer
     sens_v_res = sens_v_avg / buffer
 
-    sys.stdout.write("\r" + "Turbidity: " + str(turb_res) + ' NTU - Sens_v : ' +str(sens_volts_res) )
+    sys.stdout.write("\r" + "Turbidity: " + str(turb_res) + ' NTU - Sens_v : ' +str(sens_v_res) )
     sys.stdout.flush()
     sleep(0.5)
 
